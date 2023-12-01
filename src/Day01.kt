d0adfa34984684fcc56299942ce877d8d60c54f4
Simon Hohenwarter <113617179+simonh1701@users.noreply.github.com>
2023-12-01 11:58:43 +0100

    Day 01 - Part 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,22 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val integerChars = '0'..'9'
+
+        val calibrationValues = input
+            .map { line -> "${line.find { it in integerChars }}${line.reversed().find { it in integerChars }}" }
+            .map { it.toInt() }
+
+        return calibrationValues.reduce { acc, value -> acc + value }
     }
 
-    fun part2(input: List<String>): Int {
+    /*fun part2(input: List<String>): Int {
         return input.size
-    }
+    }*/
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = readInput("Day01_Part1_Test")
+    check(part1(testInput) == 142)
 
     val input = readInput("Day01")
     part1(input).println()
-    part2(input).println()
+    // part2(input).println()
 }
