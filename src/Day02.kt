44fb90d95b5b832717ce7491c613b59db8c55337
Simon Hohenwarter <113617179+simonh1701@users.noreply.github.com>
2023-12-02 13:24:34 +0100

    Day 02 - Part 2

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,37 +1,6 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        // Turn the input into a list of game objects
-        val games = input.map { line ->
-            // Get the ID of the game
-            val gameId: Int = line.split(": ", ignoreCase = true, limit = 2)[0].substringAfterLast(' ').toInt()
-
-            // Turn the game into a list of Draws where each draw is a set of Cubes
-            val gameString = line.split(": ", ignoreCase = true, limit = 2)[1]
-            val cubesPerDraw = gameString
-                .split("; ")
-                .map { draw ->
-                    draw.split(", ")
-                        .map { pick ->
-                            val amount = pick.substringBefore(" ").toInt()
-                            val color: Color = when (pick.substringAfter(" ")) {
-                                "blue" -> Color.BLUE
-                                "green" -> Color.GREEN
-                                "red" -> Color.RED
-                                else -> {
-                                    throw Exception("Found unexpected color")
-                                }
-                            }
-
-                            Cubes(amount, color)
-                        }
-                        .toSet()
-                }
-
-            val draws = cubesPerDraw.map { cubes -> Draw(cubes) }
-
-            Game(gameId, draws)
-        }
-
+        val games = turnInputTextIntoGameObjects(input)
 
         return games
             // Filter out all games that are impossible with the given bag of cubes that only contains 14 blue ones, 13 green ones and 12 red ones
@@ -41,21 +10,84 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val games = turnInputTextIntoGameObjects(input)
+
+        return games
+            .map { game ->
+                val minimumOfCubesPerColor = game.minimumOfCubesPerColor
+                val minimumSetOfCubes = minimumOfCubesPerColor.map { minimumOfCubes ->
+                    Cubes(
+                        amount = minimumOfCubes.value,
+                        color = minimumOfCubes.key
+                    )
+                }.toSet()
+                Draw(minimumSetOfCubes).power
+            }
+            .fold(0) { acc, power -> acc + power }
     }
 
-    val testInputPart1 = readInput("Day02_Test")
-    check(part1(testInputPart1) == 8)
+    val testInput = readInput("Day02_Test")
+    check(part1(testInput) == 8)
+    check(part2(testInput) == 2286)
 
     val input = readInput("Day02")
     part1(input).println()
-    // part2(input).println()
+    part2(input).println()
+}
+
+fun turnInputTextIntoGameObjects(input: List<String>): List<Game> {
+    // Turn the input into a list of game objects
+    val games = input.map { line ->
+        // Get the ID of the game
+        val gameId: Int = line.split(": ", ignoreCase = true, limit = 2)[0].substringAfterLast(' ').toInt()
+
+        // Turn the game into a list of Draws where each draw is a set of Cubes
+        val gameString = line.split(": ", ignoreCase = true, limit = 2)[1]
+        val cubesPerDraw = gameString
+            .split("; ")
+            .map { draw ->
+                draw.split(", ")
+                    .map { pick ->
+                        val amount = pick.substringBefore(" ").toInt()
+                        val color: Color = when (pick.substringAfter(" ")) {
+                            "blue" -> Color.BLUE
+                            "green" -> Color.GREEN
+                            "red" -> Color.RED
+                            else -> {
+                                throw Exception("Found unexpected color")
+                            }
+                        }
+
+                        Cubes(amount, color)
+                    }
+                    .toSet()
+            }
+
+        val draws = cubesPerDraw.map { cubes -> Draw(cubes) }
+
+        Game(gameId, draws)
+    }
+
+    return games
 }
 
 data class Game(val id: Int, val draws: List<Draw>) {
     val isValid: Boolean
         get() {
             return draws.all { draw -> draw.isValid }
+        }
+
+    val minimumOfCubesPerColor: Map<Color, Int>
+        get() {
+            fun maxAmountOfCubesInDrawFor(color: Color): Int = draws.maxOfOrNull { draw ->
+                draw.setOfCubes.filter { cubes -> cubes.color == color }.fold(0) { acc, cubes -> acc + cubes.amount }
+            } ?: 0
+
+            return mapOf(
+                Color.BLUE to maxAmountOfCubesInDrawFor(color = Color.BLUE),
+                Color.GREEN to maxAmountOfCubesInDrawFor(color = Color.GREEN),
+                Color.RED to maxAmountOfCubesInDrawFor(color = Color.RED)
+            )
         }
 }
 
@@ -71,6 +103,11 @@ data class Draw(val setOfCubes: Set<Cubes>) {
 
                 cubes.amount <= limit
             }
+        }
+
+    val power: Int
+        get() {
+            return setOfCubes.fold(1) { acc, cubes -> acc * cubes.amount }
         }
 }
 
