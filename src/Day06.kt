ea65622d90f0ac669c04bf3583193b9f9d3cc5f7
Simon Hohenwarter <113617179+simonh1701@users.noreply.github.com>
2023-12-06 11:31:25 +0100

    Day 06 - Part 2

--- src/Day06.kt
+++ src/Day06.kt
@@ -1,31 +1,32 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        val timesInMs = input[0].substringAfter("Time:").split(" ").filter { it.isNotBlank() }.map { it.trim() }.map(String::toInt)
-        val distancesInMm = input[1].substringAfter("Distance:").split(" ").filter { it.isNotBlank() }.map { it.trim() }.map(String::toInt)
+    fun part1(input: List<String>): Long {
+        val timesInMs = input[0].substringAfter("Time:").split(" ").filter { it.isNotBlank() }.map { it.trim() }.map(String::toLong)
+        val distancesInMm = input[1].substringAfter("Distance:").split(" ").filter { it.isNotBlank() }.map { it.trim() }.map(String::toLong)
 
         return timesInMs
                 .mapIndexed { index, time -> Pair(time, distancesInMm[index]) }
                 .map { countNumberOfWaysToBeatTheRecord(it.first, it.second) }
-                .fold(1) { acc: Int, i: Int ->
-                    acc * i
-                }
+                .fold(1) { acc: Long, i: Long -> acc * i }
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<String>): Long {
+        val timeInMs = input[0].substringAfter("Time:").split(" ").filter { it.isNotBlank() }.joinToString(separator = "") { it.trim() }.toLong()
+        val distanceInMm = input[1].substringAfter("Distance:").split(" ").filter { it.isNotBlank() }.joinToString(separator = "") { it.trim() }.toLong()
+
+        return countNumberOfWaysToBeatTheRecord(timeInMs, distanceInMm)
     }
 
     val testInput = readInput("Day06_Test")
-    check(part1(testInput) == 288)
-    // check(part2(testInput) == 0)
+    check(part1(testInput) == (288).toLong())
+    check(part2(testInput) == (71503).toLong())
 
     val input = readInput("Day06")
     part1(input).println()
-    // part2(input).println()
+    part2(input).println()
 }
 
-fun countNumberOfWaysToBeatTheRecord(raceTimeInMs: Int, recordDistanceInMm: Int): Int {
+fun countNumberOfWaysToBeatTheRecord(raceTimeInMs: Long, recordDistanceInMm: Long): Long {
     return (0..raceTimeInMs).map { buttonHoldTime ->
         buttonHoldTime * (raceTimeInMs - buttonHoldTime)
-    }.count { it > recordDistanceInMm }
+    }.count { it > recordDistanceInMm }.toLong()
 }
