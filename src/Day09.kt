be6ad55b8d75b977342263d1586c0568269ec369
Simon Hohenwarter <113617179+simonh1701@users.noreply.github.com>
2023-12-09 12:06:07 +0100

    Day 09 - Part 2

--- src/Day09.kt
+++ src/Day09.kt
@@ -5,16 +5,17 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val numberLists = input.map { line -> line.split(" ").map(String::toInt) }
+        return numberLists.sumOf { it.previous() }
     }
 
     val testInput = readInput("Day09_Test")
     check(part1(testInput) == 114)
-    // check(part2(testInput) == 0)
+    check(part2(testInput) == 2)
 
     val input = readInput("Day09")
     part1(input).println()
-    // part2(input).println()
+    part2(input).println()
 }
 
 fun List<Int>.next(): Int {
@@ -23,4 +24,12 @@ fun List<Int>.next(): Int {
     val differences = this.windowed(2, 1).map { x -> x.last() - x.first() }
 
     return this.last() + differences.next()
+}
+
+fun List<Int>.previous(): Int {
+    if (this.all { it == 0 }) return 0
+
+    val differences = this.windowed(2, 1).map { x -> x.last() - x.first() }
+
+    return this.first() - differences.previous()
 }
